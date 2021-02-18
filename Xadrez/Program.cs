b02eec0e7490d0c854ed109c161be27d9bb3856d
Jonathan <jonathanoliveira1910@gmail.com>
2021-02-18 11:23:43 -0300

    Implementando o tabuleiro

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using tabuleiro;
+using Tabuleiro;
 
 namespace Xadrez
 {
--- Xadrez/tabuleiro/Posicao.cs
+++ Xadrez/tabuleiro/Posicao.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace tabuleiro
+namespace Tabuleiro
 {
     class Posicao
     {
